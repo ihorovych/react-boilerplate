34fc13192d36c88ed582c5b3a65797ce3238d848
Roman Kavun <karomigo@gmail.com>
2018-06-21 10:14:13 +0300

    basic config

---
+++ README.md
@@ -0,0 +1,7 @@
+This is boilerplate for React.
+Basic config with ESLint with AirBnb standart.
+
+To run dev-server:
+```
+npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+};
